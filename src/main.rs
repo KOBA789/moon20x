b754b75b38cefe4fbb3636b25ad9f507d65d68ed
Hidekazu Kobayashi <kobahide789@gmail.com>
2017-07-30 00:52:01 +0900

    it works

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,24 @@
 extern crate ws;
 
-use std::rc::Rc;
-use std::cell::Cell;
+use std::sync::atomic::{AtomicUsize, Ordering};
+use std::thread;
 
 use ws::{listen, Handler, Sender, Result, Message, Handshake, CloseCode, Error};
 
-struct Server {
+struct Server<'a> {
     out: Sender,
-    count: Rc<Cell<u32>>,
+    count: &'a AtomicUsize,
 }
 
-impl Handler for Server {
+impl<'a> Handler for Server<'a> {
 
     fn on_open(&mut self, _: Handshake) -> Result<()> {
-        // We have a new connection, so we increment the connection counter
-        Ok(self.count.set(self.count.get() + 1))
+        Ok(())
     }
 
     fn on_message(&mut self, msg: Message) -> Result<()> {
-        // Tell the user the current count
-        println!("The number of live connections is {}", self.count.get());
-
-        // Echo the message back
-        self.out.send(msg)
+        self.count.fetch_add(1, Ordering::SeqCst);
+        Ok(())
     }
 
     fn on_close(&mut self, code: CloseCode, reason: &str) {
@@ -33,9 +29,6 @@ impl Handler for Server {
                 "Closing handshake failed! Unable to obtain closing status from client."),
             _ => println!("The client encountered an error: {}", reason),
         }
-
-        // The connection is going down, so we need to decrement the count
-        self.count.set(self.count.get() - 1)
     }
 
     fn on_error(&mut self, err: Error) {
@@ -45,10 +38,8 @@ impl Handler for Server {
 }
 
 fn main() {
-  // Cell gives us interior mutability so we can increment
-  // or decrement the count between handlers.
-  // Rc is a reference-counted box for sharing the count between handlers
-  // since each handler needs to own its contents.
-  let count = Rc::new(Cell::new(0));
-  listen("127.0.0.1:3012", |out| { Server { out: out, count: count.clone() } }).unwrap()
+    let count = AtomicUsize::new(0);
+    listen("127.0.0.1:3012", |out| {
+        Server { out: out, count: &count }
+    }).unwrap()
 }
